ddba19a26d7a536699866794596fb8276a096a58
泉户泽雨 <1624910218@qq.com>
2024-12-16 09:45:50 +0800

    注册渲染选择方块类

--- src/main/java/club/heiqi/qz_miner/ClientProxy.java
+++ src/main/java/club/heiqi/qz_miner/ClientProxy.java
@@ -1,10 +1,13 @@
 package club.heiqi.qz_miner;
 
 import club.heiqi.qz_miner.client.keybind.KeyBind;
+import club.heiqi.qz_miner.client.renderSelect.RenderSelect;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
     public KeyBind keyBind;
+    public RenderSelect renderSelect;
 
     // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
     // Don't forget to call the super methods as well.
@@ -13,5 +16,12 @@ public class ClientProxy extends CommonProxy {
         super.preInit(event);
         keyBind = new KeyBind();
         keyBind.register();
+        renderSelect = new RenderSelect();
+        RenderSelect.register(renderSelect);
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
     }
 }
