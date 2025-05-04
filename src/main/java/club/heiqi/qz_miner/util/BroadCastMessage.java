7bdfe0405b3ecea0a16d854fe1d44edb29065a35
泉户泽雨 <1624910218@qq.com>
2025-05-04 16:57:49 +0800

    添加工具方法广播消息给所有玩家

---
+++ src/main/java/club/heiqi/qz_miner/util/BroadCastMessage.java
@@ -0,0 +1,20 @@
+package club.heiqi.qz_miner.util;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+
+import java.util.List;
+
+public class BroadCastMessage {
+
+    public static void broadCastMessage(String text) {
+        List<EntityPlayerMP> allPlayers = FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().playerEntityList;
+        ChatComponentText CCT = new ChatComponentText(text);
+        for (EntityPlayerMP playerMP : allPlayers) {
+            playerMP.addChatMessage(CCT);
+        }
+    }
+}
